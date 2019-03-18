56793a6808a6d8166afbc73e2452b8183e79c9ac
Gordon Watts <gordon@gordonandpaula.net>
2019-03-18 15:00:24 -0400

    Using LINQ syntax.

--- 01-SimpleCut.py
+++ 01-SimpleCut.py
@@ -3,3 +3,7 @@
 good_events = events[met > 60]
 
 plt.hist(events.met, bins=50)
+
+
+# Alternate way, using the LINQ version, but should be equivalent.
+events.Where(lambda e: e.met > 60).PlotHisto(bins=50)
