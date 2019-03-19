393f4f0da6a2ae81a7d107323009f215829454d2
Gordon Watts <gordon@gordonandpaula.net>
2019-03-19 15:28:23 -0400

    missing item

--- 02-Jets.py
+++ 02-Jets.py
@@ -4,7 +4,7 @@
 all_jets = events.jets
 
 # A good jet is one that has pt greater than 40 GeV
-a_good_jet = pt > 40
+a_good_jet = all_jets.pt > 40
 good_jets = all_jets[a_good_jet]
 
 # Events with at least 2 good jets
@@ -21,7 +21,7 @@ plt.hist(good_events.jets[a_good_jet].pt)
 a_good_jet = lambda j: j.pt > 40
 
 good_events = events \
-                .Where(lambda e: e.jets.where(lambda j: a_good_jet(j)).count())
+                .Where(lambda e: e.jets.where(lambda j: a_good_jet(j)).count() > 2)
 
 good_events.SelectMany(lambda e: e.jets) \
         .Select(lambda j: j.pt) \
